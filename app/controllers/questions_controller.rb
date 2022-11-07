7eb6ea4f3fc58bc4016443871b50ea4e12d3b9bc
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-07 14:04:25 -0500

    Merci a Ines pour tout! Sans elle je serais perdu dans l'infinitude des erreurs de rails...

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,14 +4,14 @@ class QuestionsController < ApplicationController
 
   def answer
     @input = params[:input]
-    raise
+    # raise
     if @input == "I am going to work"
-      @answer == "Great!"
+      @answer = "Great!"
     elsif @input.include?('?')
-      @answer == "Silly question, get dressed and go to work!"
+      @answer = "Silly question, get dressed and go to work!"
     elsif @input != "I am going to work"
-      @answer == "I don't care, get dressed and go to work!"
-
+      @answer = "I don't care, get dressed and go to work!"
     end
+    # raise
   end
 end
