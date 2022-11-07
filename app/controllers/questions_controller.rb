be2392329d8c17d544f1d41399a46c961d6c31aa
Reed Copeland tgioml@hotmail.com <tgioml@hotmail.com>
2022-11-07 13:50:34 -0500

    pushing since Stephane told us to LMAO

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @input = params[:input]
+    raise
+    if @input == "I am going to work"
+      @answer == "Great!"
+    elsif @input.include?('?')
+      @answer == "Silly question, get dressed and go to work!"
+    elsif @input != "I am going to work"
+      @answer == "I don't care, get dressed and go to work!"
+
+    end
+  end
+end
